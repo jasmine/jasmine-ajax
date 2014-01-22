5ccd4827ee7f1170d77a2543d2f160b90d4e00a5
slackersoft <gregg@slackersoft.net>
2014-01-21 17:06:23 -1000

    allow query string for stubbed requests to be in any order

--- spec/javascripts/webmock-style-spec.js
+++ spec/javascripts/webmock-style-spec.js
@@ -1,7 +1,8 @@
 describe("Webmock style mocking", function() {
   var successSpy, errorSpy, response, fakeGlobal, mockAjax;
 
-  var sendRequest = function(fakeGlobal) {
+  var sendRequest = function(fakeGlobal, url) {
+    url = url || "http://example.com/someApi"
     var xhr = new fakeGlobal.XMLHttpRequest();
     xhr.onreadystatechange = function(arguments) {
       if (this.readyState == this.DONE) {
@@ -10,7 +11,7 @@ describe("Webmock style mocking", function() {
       }
     };
 
-    xhr.open("GET", "http://example.com/someApi");
+    xhr.open("GET", url);
     xhr.send();
   };
 
@@ -61,6 +62,18 @@ describe("Webmock style mocking", function() {
 
     it("should allow the latest stub to win", function() {
       expect(response.responseText).toEqual('no');
+    });
+  });
+
+  describe("with a query string", function() {
+    beforeEach(function() {
+      mockAjax.stubRequest("http://example.com/someApi?foo=bar&baz=quux").andReturn({responseText: "greetings", status: 422});
+    });
+
+    it("should match the query string in any order", function() {
+      sendRequest(fakeGlobal, "http://example.com/someApi?baz=quux&foo=bar");
+      expect(response.status).toEqual(422);
+      expect(response.responseText).toEqual('greetings');
     });
   });
 
