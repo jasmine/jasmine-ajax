23fb44ae687bb074a238075fcb3f9173eb906319
Alexey Volkov <a@insvit.com>
2015-07-22 15:08:35 +0300

    Update boot.js

--- src/boot.js
+++ src/boot.js
@@ -3,7 +3,7 @@
       MockAjax = jRequire.ajax(jRequire);
   if (typeof window === "undefined" && typeof exports === "object") {
     exports.MockAjax = MockAjax;
-    jasmine.Ajax = new MockAjax(exports);
+    jasmine.Ajax = new MockAjax(global);
   } else {
     window.MockAjax = MockAjax;
     jasmine.Ajax = new MockAjax(window);
