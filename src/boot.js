7cb4e19c8380aa1826b2e9d0e6dd780e806d447e
simongfxu <xugaofan@gmail.com>
2015-10-31 00:21:26 +0800

    minimum modification to adapt node.js (still uncomfortable)

--- src/boot.js
+++ src/boot.js
@@ -3,7 +3,19 @@
       MockAjax = jRequire.ajax(jRequire);
   if (typeof window === "undefined" && typeof exports === "object") {
     exports.MockAjax = MockAjax;
-    jasmine.Ajax = new MockAjax(exports);
+
+    /**
+     * jasmine is an instance of require('jasmine') in node.js
+     * we may not use jasmine to execute our specs
+     * then set it manually like this:
+     *
+     * var jas = new new Jasmine();
+     * var jasmineAjax = require('jasmine-ajax');
+     * jas.Ajax = new jasmineAjax.MockAjax(global);
+     */
+    if (typeof jasmine !== 'undefined') {
+      jasmine.Ajax = new MockAjax(this);
+    }
   } else {
     window.MockAjax = MockAjax;
     jasmine.Ajax = new MockAjax(window);
