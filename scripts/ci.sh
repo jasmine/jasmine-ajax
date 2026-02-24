462305a9d7064f476c4b08b9a1baa40de144d330
Steve Gravrock <sdg@panix.com>
2026-02-23 19:07:09 -0800

    Bring browser matrix in line with jasmine-core

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -3,12 +3,12 @@
 set -e
 ./node_modules/.bin/grunt jshint
 
-# Run tests against all supported browsers
-# (see .circleci/config.yml for that)
+# Run tests against all supported browsers that are available on Saucelabs
+# (see run-sauce-browsers for the list)
 export SAUCE_TUNNEL_NAME=$CIRCLE_WORKFLOW_JOB_ID
 scripts/start-sauce-connect
 set +o errexit
-scripts/run-all-browsers
+scripts/run-sauce-browsers
 exitcode=$?
 set -o errexit
 scripts/stop-sauce-connect
